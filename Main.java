7cd738a4173763b7bfda38f3a054038fe55b2aa0
Hamod2121 <124037111+Hamod2121@users.noreply.github.com>
2025-05-04 00:02:18 +0300

    Add input

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
         System.out.println("Have a great day!");
+        System.out.println("Goodbye for now!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What's your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Nice to meet you, " + name + "!");
     }
 }
